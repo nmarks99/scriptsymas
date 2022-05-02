945e1a4d78916b246d260f3034da606bc42bc9e6
Nick <nmarks99@gmail.com>
2022-05-02 16:50:29 -0500

    save

--- trash/main.c
+++ trash/main.c
@@ -1,11 +1,27 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
+#include <time.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+
+void getFileCreationTime(char *filePath) {
+    struct stat attrib;
+    stat(filePath, &attrib);
+    char date[10];
+    strftime(date, 10, "%d-%m-%y", gmtime(&(attrib.st_ctime)));
+    printf("The file %s was last modified at %s\n", filePath, date);
+    date[0] = 0;
+}
 
 int main(int argc, char *argv[]) {
-    
-    char cmd[50];
-    sprintf(cmd,"mv %s ~/.trash/",argv[1]);
-    system(cmd); 
+
+    get_file_modification_time("../rmspace.py");
+
+
+    // char cmd[50];
+    // sprintf(cmd,"mv %s ~/.trash/",argv[1]);
+    // system(cmd); 
     return 0;
 }
 

9451e83545dc2fbff1cf5d55b568666e66dccc55
Nick <nmarks99@gmail.com>
2022-07-05 09:03:04 -0500

    update readme.md

--- openremote/openremote.sh
+++ openremote/openremote.sh
@@ -1,10 +1,35 @@
 #!/usr/bin/env bash 
+
+
+case "$(uname -s)" in
+   Darwin)
+     OS='Mac'
+     ;;
+   Linux)
+     OS='Linux'
+     ;;
+   CYGWIN*|MINGW32*|MSYS*|MINGW*)
+     OS='Windows'
+     ;;
+   *)
+     OS='Other' 
+     ;;
+esac
+
+
 DIR=${PWD##*/}
 MATCH="GitHub"
 
-if [ "$DIR" = "$MATCH" ]; then
-   xdg-open "https://www.github.com" 
-else
-   xdg-open $(git config remote.origin.url)
+if [ "$OS" = "Windows" ]; then
+    if [ "$DIR" = "$MATCH" ]; then
+        start "https://www.github.com" 
+    else
+        start $(git config remote.origin.url)
+    fi
+elif [ "$OS" = "Windows" ]; then
+    if [ "$DIR" = "$MATCH" ]; then
+        xdg-open "https://www.github.com" 
+    else
+        xdg-open $(git config remote.origin.url)
+    fi
 fi
-

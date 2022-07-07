89ac799adc74b7c51481dbd67862ea51bbeb705c
Nick <nmarks99@gmail.com>
2022-07-07 14:22:27 -0500

    save

--- md2pdf/md2pdf.py
+++ md2pdf/md2pdf.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python
+
+# You must install pandoc 
+# You must also install pdflatex (probably with MiKTeX)
+
 import os
 import sys
 
@@ -7,17 +11,16 @@ x = sys.argv
 inmd = x[1] # Get name of markdown file
 outpdf = x[2] # Get name of output pdf
 
-# pandoc command
-cmd = ('pandoc --number-sections ' 
+pandoc_cmd = ('pandoc --number-sections ' 
 '-V geometry:paperwidth=8.5in '
 '-V geometry:paperheight=11in '
 '-V geometry:margin=1in ' 
 '-V colorlinks ' 
-'-V urlcolor=blue ')
+'-V urlcolor=blue '
+'--pdf-engine=pdflatex')
 
-cmd = cmd + ' -o {outpdf} {inmd}'.format(outpdf=outpdf,inmd=inmd)
-os.system('clear||cls')
+pandoc_cmd = "".join([pandoc_cmd,' -o {outpdf} {inmd}'.format(outpdf=outpdf,inmd=inmd)])
+os.system("clear|cls")
 print('Generating PDF...')
-os.system(cmd)
-os.system('clear||cls')
+os.system(pandoc_cmd)
 print('Done')
